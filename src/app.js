969789757cfed0066cbaa07fc999622974619230
kacijcox <145575906+kacijcox@users.noreply.github.com>
2023-11-04 00:38:47 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const generateDomain = () => {
+  let pronoun = ["my", "our"];
+  let adj = ["awful", "polite"];
+  let noun = ["dolphin", "cheeto"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  for (let i = 0; i < pronoun.length; i++)
+    for (let y = 0; y < adj.length; y++)
+      for (let x = 0; x < noun.length; x++)
+        console.log(pronoun[i] + adj[y] + noun[x] + ".com");
 };
+generateDomain();
